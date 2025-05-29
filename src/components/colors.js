add0f8feab247e44a4022f36fae3f2837e392c10
mauricio <hmcerruto@umsa.bo>
2025-05-28 22:38:43 -0400

    Create colors.js

---
+++ src/components/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: "#264653",
+  secondary: "#2a9d8f",
+  accent: "#e9c46a",
+  highlight: "#f4a261",
+  background: "#e9f5f9",
+  error: "#e76f51",
+  success: "#2a9d8f",
+};
